9bafc8f9f083fe2e295de7650188b6c24c881b4b
furkannhayran <furkannhyrn@hotmail.com>
2023-02-12 18:59:08 +0300

    Add project files.

---
+++ MvcMovie/Models/ErrorViewModel.fs
@@ -0,0 +1,9 @@
+namespace MvcMovie.Models
+
+open System
+
+type ErrorViewModel =
+    { RequestId: string }
+
+    member this.ShowRequestId =
+        not (String.IsNullOrEmpty(this.RequestId))
---
+++ MvcMovie/wwwroot/js/site.js
@@ -0,0 +1,24 @@
+﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+
+const options = {
+
+    position: "absolute", // default: '32px'
+    top: "0px",
+    bottom:"82%",
+    right: '15px', // default: '32px'
+    time: '0.5s', // default: '0.3s'
+    mixColor: '#fff', // default: '#fff'
+    backgroundColor: '#fff',  // default: '#fff'
+    buttonColorDark: '#100f2c',  // default: '#100f2c'
+    buttonColorLight: '#fff', // default: '#fff'
+    saveInCookies: false, // default: true,
+    label: '🌓', // default: ''
+    autoMatchOsTheme: true // default: true
+}
+
+function addDarkmodeWidget() {
+    const dark = new Darkmode(options);
+
+    dark.showWidget();
+}
+window.addEventListener('load', addDarkmodeWidget);
